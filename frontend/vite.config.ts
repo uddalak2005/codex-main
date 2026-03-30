68dd7d619c88038f4451adc6bb22a6b9cafbf06d
Uddalak Mukhopadhyay <uddalakmukhopadhyay@gmail.com>
2026-03-31 02:09:09 +0530

    Code phatt gya -- silh diya

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import path from "path"
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://codex-iter.in',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/blogs',
+        '/events',
+        '/team'
+      ],
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  build: {
+    chunkSizeWarningLimit: 800, // optional (default 500)
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor'
+            }
+            if (id.includes('react-router')) {
+              return 'router'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
